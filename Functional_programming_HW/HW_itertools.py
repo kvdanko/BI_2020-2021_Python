32460de0c205bc354bd83360f489da4db885aca0
kvdanko <70286366+kvdanko@users.noreply.github.com>
2021-04-30 00:41:21 +0300

    Fixed

--- Functional_programming_HW/HW_itertools.py
+++ Functional_programming_HW/HW_itertools.py
@@ -2,11 +2,9 @@ import itertools
 
 
 def combinations(n):
-    comb_list = []
     for i in range(1, n + 1):
-        comb_list += itertools.product("AGTC", repeat=i)
-    for tuples in comb_list:
-        yield ''.join(tuples)
+        for seq in itertools.product("AGTC", repeat=i):
+            yield ''.join(seq)
 
 
 print(list(combinations(3)))
